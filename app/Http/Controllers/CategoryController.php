bb344aed369d9d8d0faf5905bbbcb3bbfcb61bf6
Juan Carlos Vazquez Gracia <juancarlosvazquezgarcia07@gmail.com>
2022-04-08 16:58:56 -0500

    Adding functionality for creating category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
