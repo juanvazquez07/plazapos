e784aa2d15e0212bc869b9b592b248484cd4d51c
Juan Carlos Vazquez Gracia <juancarlosvazquezgarcia07@gmail.com>
2022-06-09 19:46:56 -0500

    Cambio colores inicio

--- public/js/app.js
+++ public/js/app.js
@@ -23265,14 +23265,34 @@ var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
 );
 
 var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("section", {
-  "class": "bg-white dark:bg-gray-900"
+  "class": "font-bold bg-white dark:bg-gray-900 text-white"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   "class": "grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
   href: ""
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   style: {
-    "background-color": "#79e3aa"
+    "background-color": "#895EF7"
+  },
+  "class": "container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "p-6"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h1", null, "Nueva venta"), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("svg", {
+  "class": "w-6 h-6",
+  fill: "none",
+  stroke: "currentColor",
+  viewBox: "0 0 24 24",
+  xmlns: "http://www.w3.org/2000/svg"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("path", {
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round",
+  "stroke-width": "2",
+  d: "M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+})])])])]), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
+  href: ""
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  style: {
+    "background-color": "#6A84ED"
   },
   "class": "container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
@@ -23281,7 +23301,7 @@ var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
   href: ""
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   style: {
-    "background-color": "#79e3aa"
+    "background-color": "#4DA2F8"
   },
   "class": "container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
@@ -23290,7 +23310,7 @@ var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
   href: ""
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   style: {
-    "background-color": "#79e3aa"
+    "background-color": "#1FCBDC"
   },
   "class": "container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
@@ -23299,16 +23319,7 @@ var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
   href: ""
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   style: {
-    "background-color": "#79e3aa"
-  },
-  "class": "container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
-}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
-  "class": "p-6"
-}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h1", null, "Nueva venta")])])]), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
-  href: ""
-}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
-  style: {
-    "background-color": "#79e3aa"
+    "background-color": "#59C894"
   },
   "class": "container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,38 +7,39 @@
                 Dashboard
             </h2>
         </template>
-        <section class="bg-white dark:bg-gray-900">
+        <section class="font-bold bg-white dark:bg-gray-900 text-white">
             <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 ">
                 <a href="">
-                    <div style="background-color:#79e3aa;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
+                    <div style="background-color:#895EF7;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
+                        <div class="p-6">
+                            <h1>Nueva venta</h1>
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </div>
+                    </div>
+                </a>
+                <a href="">
+                    <div style="background-color:#6A84ED;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
                         <div class="p-6">
                             <h1>Nueva venta</h1>
                         </div>
                     </div>
                 </a>
                 <a href="">
-                    <div style="background-color:#79e3aa;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
+                    <div style="background-color:#4DA2F8;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
                         <div class="p-6">
                             <h1>Nueva venta</h1>
                         </div>
                     </div>
                 </a>
                 <a href="">
-                    <div style="background-color:#79e3aa;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
+                    <div style="background-color:#1FCBDC;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
                         <div class="p-6">
                             <h1>Nueva venta</h1>
                         </div>
                     </div>
                 </a>
                 <a href="">
-                    <div style="background-color:#79e3aa;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
-                        <div class="p-6">
-                            <h1>Nueva venta</h1>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div style="background-color:#79e3aa;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
+                    <div style="background-color:#59C894;" class="container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300">
                         <div class="p-6">
                             <h1>Nueva venta</h1>
                         </div>
